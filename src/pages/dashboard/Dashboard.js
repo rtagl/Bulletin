da2c9bad494c5497a40b42b1574fe0ebad5a8f65
Ric Taglietti <rictaglietti@gmail.com>
2022-02-01 14:10:20 -0500

    add search filter

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,16 +1,57 @@
+import { useState } from 'react';
 import { useCollection } from '../../hooks/useCollection';
+import { useAuthContext } from '../../hooks/useAuthContext';
 import ProjectList from '../../components/ProjectList';
+import ProjectFilter from './ProjectFilter';
 
 // styles
 import './Dashboard.css';
 
 export default function Dashboard() {
   const { documents, error } = useCollection('projects');
+  const { user } = useAuthContext();
+  const [currentFilter, setCurrentFilter] = useState('all');
+
+  const changeFilter = (newFilter) => {
+    setCurrentFilter(newFilter);
+  };
+
+  const filteredProjects = documents
+    ? documents.filter((document) => {
+        switch (currentFilter) {
+          case 'all':
+            return true;
+          case 'mine':
+            let assignedToMe = false;
+            document.assignedUsersList.forEach((u) => {
+              if (user.uid === u.id) {
+                assignedToMe = true;
+              }
+            });
+            return assignedToMe;
+          case 'development':
+          case 'design':
+          case 'sales':
+          case 'marketing':
+            console.log(document.category, currentFilter);
+            return document.category === currentFilter;
+          default:
+            return true;
+        }
+      })
+    : null;
+
   return (
     <div>
       <h2 className="page-title">Projects</h2>
       {error && <p className="error">{error}</p>}
-      {documents && <ProjectList projects={documents} />}
+      {documents && (
+        <ProjectFilter
+          currentFilter={currentFilter}
+          changeFilter={changeFilter}
+        />
+      )}
+      {filteredProjects && <ProjectList projects={filteredProjects} />}
     </div>
   );
 }
---
+++ src/pages/dashboard/ProjectFilter.js
@@ -0,0 +1,29 @@
+const filterList = [
+  'all',
+  'mine',
+  'development',
+  'design',
+  'marketing',
+  'sales',
+];
+
+export default function ProjectFilter({ currentFilter, changeFilter }) {
+  const handleClick = (newFilter) => {
+    changeFilter(newFilter);
+  };
+
+  return (
+    <div className="project-filter">
+      <nav>
+        {filterList.map((f) => (
+          <button
+            key={f}
+            className={currentFilter === f ? 'active' : ''}
+            onClick={() => handleClick(f)}>
+            {f}
+          </button>
+        ))}
+      </nav>
+    </div>
+  );
+}
